899e954a694014c4de3e94352314b9e3d0dce37e
Soravis Prakkamakul <asunnotthesun@gmail.com>
2015-12-16 23:34:36 +0700

    Revise senze Thai and Eng Keyboard Layout

---
+++ apps/keyboard/js/layouts/th-senze-lexical.js
@@ -0,0 +1,100 @@
+Keyboards['th-senze-lexical'] = {
+  label: 'Thai',
+  shortLabel: 'Th',
+  menuLabel: 'ไทย', /*ไทย*/
+  basicLayoutKey: 'กขค', /*กขค*/
+  imEngine: 'thai',
+  types: ['text', 'url', 'email'],
+  autoCorrectLanguage: 'th',
+  autoCorrectPunctuation: false,
+  lang: 'th-senze',
+  disableAlternateLayout: true,
+  pages: [ 
+    {                    // default page
+      width: 11,
+      secondLayout: true,
+      keys: [
+        [
+          { value: 'ก' }, { value: 'ฃ' }, { value: 'ค' }, { value: 'ง' },
+          { value: 'จ' }, { value: 'ฉ' }, { value: 'ช' }, { value: 'ด' },
+          { value: 'ต' }, { value: 'ถ' }, { value: 'ท' }
+        ], [
+          { value: 'ฟ' }, { value: 'น' }, { value: 'บ' }, { value: 'ป' }, 
+          { value: 'ผ' }, { value: 'พ' }, { value: 'ม' }, { value: 'ย' }, 
+          { value: 'ร' }, { value: 'ล' }, { value: 'ว' }
+        ], [
+          { value: 'ส' }, { value: 'ห' }, { value: 'อ' }, { value: 'ิ' },
+          { value: 'ี' }, { value: 'ึ' }, { value: 'ื' }, { value: '่' },
+          { value: '้' }, { value: 'เ' }, { value: 'แ' }
+        ], [
+          /* { value: '<<', keyCode: KeyEvent.DOM_VK_ALT, targetPage: 2 },*/
+          { value: 'อื่นๆ', ratio: 1,keyCode: KeyEvent.DOM_VK_ALT, targetPage: 1 },
+          { value: '&nbsp', ratio: 6, keyCode: KeyboardEvent.DOM_VK_SPACE },
+          { value: 'ะ', ratio: 1 },
+          { value: 'า', ratio: 1 },
+          { value: '⌫', ratio: 1, keyCode: KeyEvent.DOM_VK_BACK_SPACE },
+          { value: '↵', ratio: 1, keyCode: KeyEvent.DOM_VK_RETURN }
+        ], [
+          { value: '×', ratio: 10, keyCode: KeyEvent.DOM_VK_CANCEL }
+        ]
+      ]
+    },
+    { // Char 2
+      width: 11,
+      secondLayout: true,
+      keys: [
+        [
+          { value: 'ฆ' }, { value: 'ซ' }, { value: 'ฌ' }, { value: 'ฎ' },
+          { value: 'ฏ' }, { value: 'ฐ' }, { value: 'ฑ' }, { value: 'ฒ' },
+          { value: 'ณ' }, { value: 'ฝ' }, { value: 'ธ' }
+        ], [
+          { value: 'ศ' }, { value: 'ษ' }, { value: 'ฬ' }, { value: 'ฮ' }, 
+          { value: '๊' }, { value: '๋' }, { value: '็' }, { value: 'โ' }, 
+          { value: 'ั' }, { value: 'ำ' }, { value: 'ใ' }
+        ], [
+          { value: 'ไ' }, { value: 'ๆ' }, { value: '์' }, { value: 'ู' },
+          { value: 'ุ' }, { value: 'ฤ' }, { value: 'ฦ' }, { value: '!' },
+          { value: '?' }, { value: '@' }, { value: '=' }
+        ], [
+          /* { value: '<<', keyCode: KeyEvent.DOM_VK_ALT, targetPage: 2 },*/
+          { value: 'อื่นๆ', ratio: 1,keyCode: KeyEvent.DOM_VK_ALT, targetPage: 2 },
+          { value: '&nbsp', ratio: 6, keyCode: KeyboardEvent.DOM_VK_SPACE },
+          { value: ',', ratio: 1 },
+          { value: '/', ratio: 1 },
+          { value: '⌫', ratio: 1, keyCode: KeyEvent.DOM_VK_BACK_SPACE },
+          { value: '↵', ratio: 1, keyCode: KeyEvent.DOM_VK_RETURN }
+        ], [
+          { value: '×', ratio: 10, keyCode: KeyEvent.DOM_VK_CANCEL }
+        ]
+      ]
+    },
+    { // Signs Page
+      width: 11,
+      secondLayout: true,
+      keys: [
+        [
+          { value: '1' }, { value: '2' }, { value: '3' }, { value: '4' },
+          { value: '5' }, { value: '6' }, { value: '7' }, { value: '8' },
+          { value: '9' }, { value: '0' }, { value: '=' }
+        ], [
+          { value: '@' }, { value: '#' }, { value: '฿' }, { value: '%' }, 
+          { value: '&' }, { value: '-' }, { value: '+' }, { value: '(' }, 
+          { value: ')' }, { value: '*' }, { value: '"' }
+        ], [
+          { value: '\'' }, { value: ':' }, { value: ';' }, { value: '!' },
+          { value: '?' },  { value: '<' }, { value: '>' }, 
+          { value: '{' }, { value: '}' }, { value: '[' },  { value: ']' }
+        ], [
+          { value: 'อื่นๆ', ratio: 1,keyCode: KeyEvent.DOM_VK_ALT, targetPage: 0 },
+          { value: '&nbsp', ratio: 6, keyCode: KeyboardEvent.DOM_VK_SPACE },
+          { value: ',', ratio: 1 },
+          { value: '/', ratio: 1 },
+          { value: '⌫', ratio: 1, keyCode: KeyEvent.DOM_VK_BACK_SPACE },
+          { value: '↵', ratio: 1, keyCode: KeyEvent.DOM_VK_RETURN }
+        ], [
+          { value: '×', ratio: 10, keyCode: KeyEvent.DOM_VK_CANCEL }
+        ]
+      ]
+    }
+  ]
+};
--- apps/keyboard/js/layouts/th-senze.js
+++ apps/keyboard/js/layouts/th-senze.js
@@ -7,94 +7,83 @@ Keyboards['th-senze'] = {
   types: ['text', 'url', 'email'],
   autoCorrectLanguage: 'th',
   autoCorrectPunctuation: false,
-  lang: 'th-senze',
-  disableAlternateLayout: true,
-  pages: [ 
-    {                    // default page
-      width: 11,
-      secondLayout: true,
-      keys: [
-        [
-          { value: 'ก' }, { value: 'ฃ' }, { value: 'ค' }, { value: 'ง' },
-          { value: 'จ' }, { value: 'ฉ' }, { value: 'ช' }, { value: 'ด' },
-          { value: 'ต' }, { value: 'ถ' }, { value: 'ท' }
-        ], [
-          { value: 'ฟ' }, { value: 'น' }, { value: 'บ' }, { value: 'ป' }, 
-          { value: 'ผ' }, { value: 'พ' }, { value: 'ม' }, { value: 'ย' }, 
-          { value: 'ร' }, { value: 'ล' }, { value: 'ว' }
-        ], [
-          { value: 'ส' }, { value: 'ห' }, { value: 'อ' }, { value: 'ิ' },
-          { value: 'ี' }, { value: 'ึ' }, { value: 'ื' }, { value: '่' },
-          { value: '้' }, { value: 'เ' }, { value: 'แ' }
-        ], [
-          /* { value: '<<', keyCode: KeyEvent.DOM_VK_ALT, targetPage: 2 },*/
-          { value: 'อื่นๆ', ratio: 1,keyCode: KeyEvent.DOM_VK_ALT, targetPage: 1 },
-          { value: '&nbsp', ratio: 6, keyCode: KeyboardEvent.DOM_VK_SPACE },
-          { value: 'ะ', ratio: 1 },
-          { value: 'า', ratio: 1 },
-          { value: '⌫', ratio: 1, keyCode: KeyEvent.DOM_VK_BACK_SPACE },
-          { value: '↵', ratio: 1, keyCode: KeyEvent.DOM_VK_RETURN }
-        ], [
-          { value: '', ratio: 10 }
-        ]
-      ]
+  lang: 'th',
+  pages: [ {                    // default page
+    width: 11,
+    secondLayout: true,
+    alt: {
+      'ช': ['๏', '๚', '๛']
     },
-    { // Char 2
-      width: 11,
-      secondLayout: true,
-      keys: [
-        [
-          { value: 'ฆ' }, { value: 'ซ' }, { value: 'ฌ' }, { value: 'ฎ' },
-          { value: 'ฏ' }, { value: 'ฐ' }, { value: 'ฑ' }, { value: 'ฒ' },
-          { value: 'ณ' }, { value: 'ฝ' }, { value: 'ธ' }
-        ], [
-          { value: 'ศ' }, { value: 'ษ' }, { value: 'ฬ' }, { value: 'ฮ' }, 
-          { value: '๊' }, { value: '๋' }, { value: '็' }, { value: 'โ' }, 
-          { value: 'ั' }, { value: 'ำ' }, { value: 'ใ' }
-        ], [
-          { value: 'ไ' }, { value: 'ๆ' }, { value: '์' }, { value: 'ู' },
-          { value: 'ุ' }, { value: 'ฤ' }, { value: 'ฦ' }, { value: '!' },
-          { value: '?' }, { value: '@' }, { value: '=' }
-        ], [
-          /* { value: '<<', keyCode: KeyEvent.DOM_VK_ALT, targetPage: 2 },*/
-          { value: 'อื่นๆ', ratio: 1,keyCode: KeyEvent.DOM_VK_ALT, targetPage: 2 },
-          { value: '&nbsp', ratio: 6, keyCode: KeyboardEvent.DOM_VK_SPACE },
-          { value: ',', ratio: 1 },
-          { value: '/', ratio: 1 },
-          { value: '⌫', ratio: 1, keyCode: KeyEvent.DOM_VK_BACK_SPACE },
-          { value: '↵', ratio: 1, keyCode: KeyEvent.DOM_VK_RETURN }
-        ], [
-          { value: '', ratio: 10 }
-        ]
+    keys: [
+      [
+        { value: 'ๅ' }, { value: 'ฃ' }, { value: 'ภ' }, { value: 'ถ' },
+        { value: 'ุ' }, { value: 'ึ' }, { value: 'ค' }, { value: 'ต' },
+        { value: 'จ' }, { value: 'ข' },
+        { value: 'ช', className: 'alternate-indicator' }
+      ], [
+        { value: 'ไ' }, { value: 'ำ' }, { value: 'พ' }, { value: 'ะ' },
+        { value: 'ั' }, { value: 'ี' }, { value: 'ร' }, { value: 'น' },
+        { value: 'ย' }, { value: 'บ' }, { value: 'ล' }
+      ], [
+        { value: 'ฟ' }, { value: 'ห' }, { value: 'ก' }, { value: 'ด' },
+        { value: 'เ' }, { value: '้' }, { value: '่' }, { value: 'า' },
+        { value: 'ส' }, { value: 'ว' }, { value: 'ง' }
+      ], [
+        { value: '⇪', keyCode: KeyEvent.DOM_VK_CAPS_LOCK },
+        { value: 'ป' }, { value: 'แ' }, { value: 'อ' }, { value: 'ิ' },
+        { value: 'ื' }, { value: 'ท' }, { value: 'ม' },
+        { value: 'ใ' }, { value: 'ฝ' },
+        { value: '⌫', keyCode: KeyEvent.DOM_VK_BACK_SPACE }
+      ], [
+        { value: '&nbsp', ratio: 6, keyCode: KeyboardEvent.DOM_VK_SPACE },
+        { value: '×', ratio: 2, keyCode: KeyEvent.DOM_VK_CANCEL },
+        { value: '↵', ratio: 2, keyCode: KeyEvent.DOM_VK_RETURN }
       ]
-    },
-    { // Signs Page
-      width: 11,
-      secondLayout: true,
-      keys: [
-        [
-          { value: '1' }, { value: '2' }, { value: '3' }, { value: '4' },
-          { value: '5' }, { value: '6' }, { value: '7' }, { value: '8' },
-          { value: '9' }, { value: '0' }, { value: '=' }
-        ], [
-          { value: '@' }, { value: '#' }, { value: '฿' }, { value: '%' }, 
-          { value: '&' }, { value: '-' }, { value: '+' }, { value: '(' }, 
-          { value: ')' }, { value: '*' }, { value: '"' }
-        ], [
-          { value: '\'' }, { value: ':' }, { value: ';' }, { value: '!' },
-          { value: '?' },  { value: '<' }, { value: '>' }, 
-          { value: '{' }, { value: '}' }, { value: '[' },  { value: ']' }
-        ], [
-          { value: 'อื่นๆ', ratio: 1,keyCode: KeyEvent.DOM_VK_ALT, targetPage: 0 },
-          { value: '&nbsp', ratio: 6, keyCode: KeyboardEvent.DOM_VK_SPACE },
-          { value: ',', ratio: 1 },
-          { value: '/', ratio: 1 },
-          { value: '⌫', ratio: 1, keyCode: KeyEvent.DOM_VK_BACK_SPACE },
-          { value: '↵', ratio: 1, keyCode: KeyEvent.DOM_VK_RETURN }
-        ], [
-          { value: '', ratio: 10 }
-        ]
-      ]
+    ],
+    upperCase: {
+      'ๅ': '๑',
+      'ฃ': '๒',
+      'ภ': '๓',
+      'ถ': '๔',
+      'ุ': '๕',
+      'ึ': 'ู',
+      'ค': '๖',
+      'ต': '๗',
+      'จ': '๘',
+      'ข': '๙',
+      'ช': '๐',
+      'ไ': 'ๆ',
+      'ำ': 'ฎ',
+      'พ': 'ฑ',
+      'ะ': 'ธ',
+      'ั': 'ํ',
+      'ี': '๊',
+      'ร': 'ณ',
+      'น': 'ฯ',
+      'ย': 'ญ',
+      'บ': 'ฐ',
+      'ล': 'ฅ',
+      'ฟ': 'ฤ',
+      'ห': 'ฆ',
+      'ก': 'ฏ',
+      'ด': 'โ',
+      'เ': 'ฌ',
+      '้': '็',
+      '่': '๋',
+      'า': 'ษ',
+      'ส': 'ศ',
+      'ว': 'ซ',
+      'ง': ',',
+      'ป': '฿',
+      'แ': 'ผ',
+      'อ': 'ฉ',
+      'ิ': 'ฮ',
+      'ื': 'ฺ',
+      'ท': '์',
+      'ม': 'ฒ',
+      'ใ': 'ฬ',
+      'ฝ': 'ฦ'
+      }
     }
   ]
 };
